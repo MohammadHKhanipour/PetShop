5f83f3ecf1293e69092e61b84805cf3bbe8b924f
Mohammad Hassan Khanipour <mohammadhkhanipour@outlook.com>
2022-01-12 12:54:41 +0330

    Fixed Formatting

--- PetShop.Data/Dtos/PetDto.cs
+++ PetShop.Data/Dtos/PetDto.cs
@@ -3,7 +3,7 @@
     public class PetDto : BaseDto
     {
         public Category Category { get; set; }
-        public string breed { get; set; }
+        public string Breed { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
         public string Description { get; set; }
