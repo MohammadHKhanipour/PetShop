90fe2591ae67ba8c9f2df7626d0c39b027535ade
Mohammad Hassan Khanipour <mohammadhkhanipour@outlook.com>
2022-01-12 17:36:58 +0330

    Added Repositories

--- PetShop.Data/GlobalUsings.cs
+++ PetShop.Data/GlobalUsings.cs
@@ -1,2 +1,4 @@
 ﻿global using PetShop.Data.Base;
 global using PetShop.Data.Enums;
+global using PetShop.Data.Dtos;
+global using PetShop.Data.Models;
---
+++ PetShop.Data/Repositories/ICommandRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace PetShop.Data.Repositories
+{
+    public interface ICommandRepository<Model> where Model : BaseModel
+    {
+        public Task<bool> AddAsync(Model entity);
+        public Task<bool> AddRangeAsync(List<Model> entities);
+        public Task<bool> DeleteAsync(int id);
+        public Task<bool> DeleteAsync(Model entity);
+        public Task<bool> DeleteRangeAsync(List<Model> entities);
+        public Task<bool> UpdateAsync(Model entity);
+    }
+}
